7e200148510d349dd82492886ef61b18b9159e1d
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-09 17:06:03 +0700

    Add tea-inside-bot-s4.php

---
+++ public/circleci/tea-inside-bot-s4.php
@@ -0,0 +1,14 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	$cmd = [
+		"git pull -vvv --rebase"
+	];
+
+	foreach ($cmd as $c) {
+		$c = escapeshellarg($c);
+		print shell_exec(
+			"cd /var/www/bot && sudo -u root /usr/bin/env bash -c {$c} 2>&1"
+		);
+	}	
+}
