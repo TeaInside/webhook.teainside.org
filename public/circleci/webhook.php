a18f494754cae2fb36ad84aec37b1749ba003c33
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-09 17:08:31 +0700

    Update deployer

---
+++ public/circleci/webhook.php
@@ -0,0 +1,14 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	$cmd = [
+		"git pull -vvv --rebase"
+	];
+
+	foreach ($cmd as $c) {
+		$c = escapeshellarg($c);
+		print shell_exec(
+			"cd /var/www/webhook.teainside.org && sudo -u root /usr/bin/env bash -c {$c} 2>&1"
+		);
+	}	
+}
