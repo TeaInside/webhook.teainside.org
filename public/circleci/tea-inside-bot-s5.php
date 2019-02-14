829b2eccbd2d6f642dad8a54ed6b4825e0f601d0
Ammar Faizi <ammarfaizi2@gmail.com>
2019-02-14 12:51:46 +0700

    Update webhook

--- public/circleci/tea-inside-bot-s5.php
+++ public/circleci/tea-inside-bot-s5.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 	foreach ($cmd as $c) {
 		$c = escapeshellarg($c);
 		print shell_exec(
-			"cd /var/www/bot-s5 && sudo -u root /usr/bin/env bash -c {$c} 2>&1"
+			"cd /var/www/teainside-bot-s5.teainside.org && sudo -u root /usr/bin/env bash -c {$c} 2>&1"
 		);
 	}	
 }
